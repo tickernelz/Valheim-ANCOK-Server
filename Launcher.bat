d466ee88164f097d60f4aaf5a36f0a919749302f
tickernelz <zhafronadani@gmail.com>
2021-05-01 06:19:22 +0700

    Ver. Server 1.0.9 - Update Launcher - Penambahan Tas - Fix Camera Zoom - Pengoptimalan Darah/HP dan Stamina Player - Update CraftyCarts Mod - Obor Stand dan Tembok, Otomatis Mati pada pagi hari - Bisa menggendong orang (Hotkey sama dengan naik hewan taming) - Trader berani beli mahal! - Penambahan Auto Pickup Selector (Tutorial ada di Discord) - Penambahan Item Atos Arrow (Anak Panah)

--- Launcher.bat
+++ Launcher.bat
@@ -3,15 +3,16 @@
 SETLOCAL
 SET "plugins=BepInEx\plugins"
 SET "config=BepInEx\config"
-SET "keepconfig1=ValheimFPSBoost"
-SET "keepplugins1=ValheimFPSBoost"
-SET "keepfile="
+SET "keepfolderconfig1="
+SET "keepfileconfig1="
+SET "keepfolderplugins1=ValheimFPSBoost"
+SET "keepfileplugins1="
 ECHO Updating Valheim ANCOK
 ECHO ------------------------
-FOR /d %%a IN ("%config%\*") DO IF /i NOT "%%~nxa"=="%keepconfig1%" RD /S /Q "%%a"
-FOR %%a IN ("%config%\*") DO IF /i NOT "%%~nxa"=="%keepfile%" DEL "%%a"
-FOR /d %%a IN ("%plugins%\*") DO IF /i NOT "%%~nxa"=="%keepplugins1%" RD /S /Q "%%a"
-FOR %%a IN ("%plugins%\*") DO IF /i NOT "%%~nxa"=="%keepfile%" DEL "%%a"
+FOR /d %%a IN ("%config%\*") DO IF /i NOT "%%~nxa"=="%keepfolderconfig1%" RD /S /Q "%%a"
+FOR %%a IN ("%config%\*") DO IF /i NOT "%%~nxa"=="%keepfileconfig1%" DEL "%%a"
+FOR /d %%a IN ("%plugins%\*") DO IF /i NOT "%%~nxa"=="%keepfolderplugins1%" RD /S /Q "%%a"
+FOR %%a IN ("%plugins%\*") DO IF /i NOT "%%~nxa"=="%keepfileplugins1%" DEL "%%a"
 set GIT_PATH="updater\bin\git.exe"
 set BRANCH = "origin"
 %GIT_PATH% pull %BRANCH%
